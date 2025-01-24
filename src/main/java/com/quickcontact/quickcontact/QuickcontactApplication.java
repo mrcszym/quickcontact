e6300cad0b39d90c2e9b366c5b25dd8a94d6c428
szymon <marciniecszymon00@gmail.com>
2025-01-24 19:20:01 +0100

    Initial commit

---
+++ src/main/java/com/quickcontact/quickcontact/QuickcontactApplication.java
@@ -0,0 +1,13 @@
+package com.quickcontact.quickcontact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickcontactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickcontactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quickcontact/quickcontact/QuickcontactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quickcontact.quickcontact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcontactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
